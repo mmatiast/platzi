964753fd98a26ed9cad78128c8cafeb1f1aa7622
Matias <mmatiast@miumg.edu.gt>
2021-12-14 22:21:57 -0600

    crear dise;o con tailwindcss

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function getExcerptAttribute(){
+        return substr($this->content, 0, 120); 
+    }
+    public function getPublishedAtAttribute(){
+        return $this->created_at->format('d/m/y');
+    }
 }
